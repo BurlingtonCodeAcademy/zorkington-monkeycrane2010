edad53afe91ce10d41e4649d27f3081d4bb47b92
Tania <tanialasell@gmail.com>
2020-10-29 22:31:16 -0400

    just practice how to commit

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ async function start() {
   const welcomeMessage = `182 Main St.
 You are standing on Main Street between Church and South Winooski.
 There is a door here. A keypad sits on the handle.
-On the door is a handwritten sign.`;
+On the door is a handwritten sign.
+>_`;
   let answer = await ask(welcomeMessage);
   console.log('Now write your code to make this work!');
   process.exit();
